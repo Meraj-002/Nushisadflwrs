9e6d2c7fc00d6b533204f6fc13cf26541c101079
Meraj Alam <techymeraj@gmail.com>
2026-04-13 20:37:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 //     nameContainer.appendChild(span);
 // });
 
-const name = "Nushi 🌙";
+const name = "My Wifeyy 🌙";
 const nameContainer = document.getElementById("nameText");
 
 let charIndex = 0;
